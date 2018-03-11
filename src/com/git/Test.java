2d21dd1c31e5b03f0bdea7a5ff7905b9c8d87b01
长孙歌澜 <@DESKTOP-BGA4RIR.lan>
2018-03-11 17:44:09 +0800

    3

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -4,7 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("zhangsungelan");
 		System.out.println("zhangsungelan¹þ¹þ¹þ");
 	}
 
